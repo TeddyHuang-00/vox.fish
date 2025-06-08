cf46ebc56204437e2ce0cdacee5a6bc8d51996bd
Nan Huang <teddyhuangnan@gmail.com>
2025-06-08 15:27:06 -0500

    Add python version to completion

--- completions/vox.fish
+++ completions/vox.fish
@@ -2,14 +2,14 @@
 complete -c vox -f
 
 # Main subcommands with descriptions
-complete -c vox -n __fish_use_subcommand -a "new" -d "Create a new virtual environment"
-complete -c vox -n __fish_use_subcommand -a "remove" -d "Remove an existing virtual environment"
-complete -c vox -n __fish_use_subcommand -a "activate" -d "Activate a virtual environment"
-complete -c vox -n __fish_use_subcommand -a "deactivate" -d "Deactivate the current virtual environment"
+complete -c vox -n __fish_use_subcommand -a new -d "Create a new virtual environment"
+complete -c vox -n __fish_use_subcommand -a remove -d "Remove an existing virtual environment"
+complete -c vox -n __fish_use_subcommand -a activate -d "Activate a virtual environment"
+complete -c vox -n __fish_use_subcommand -a deactivate -d "Deactivate the current virtual environment"
 
 # Environment name completions for remove and activate commands
-complete -c vox -n "__fish_seen_subcommand_from remove" -a "(__vox_complete_envs)" -d "Virtual environment"
-complete -c vox -n "__fish_seen_subcommand_from activate" -a "(__vox_complete_envs)" -d "Virtual environment"
+complete -c vox -n "__fish_seen_subcommand_from remove" -a "(__vox_complete_envs)"
+complete -c vox -n "__fish_seen_subcommand_from activate" -a "(__vox_complete_envs)"
 
 # New command should not complete files
 complete -c vox -n "__fish_seen_subcommand_from new" -f
--- functions/__vox_complete_envs.fish
+++ functions/__vox_complete_envs.fish
@@ -1,5 +1,19 @@
 function __vox_complete_envs --description "List available virtual environments for completion"
-    if test -d "$VOX_VENV_DIR"
-        ls "$VOX_VENV_DIR" 2>/dev/null
+    if not test -d "$VOX_VENV_DIR"
+        return 1
+    end
+    for env in (ls "$VOX_VENV_DIR" 2>/dev/null)
+        set config_file "$VOX_VENV_DIR/$env/pyvenv.cfg"
+        if not test -f "$config_file"
+            echo "$env"
+            continue
+        end
+        # Extract Python version from pyvenv.cfg
+        set python_version (grep "version_info = " "$config_file" | cut -d' ' -f3 | head -1)
+        if test -n "$python_version"
+            printf "%s\tPython %s\n" "$env" "$python_version"
+        else
+            echo "$env"
+        end
     end
 end
