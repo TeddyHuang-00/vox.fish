94dc6ac42c42afbf6a1b601ab31d8757be7b1718
Nan Huang <teddyhuangnan@gmail.com>
2025-06-08 15:14:24 -0500

    Add descriptions to autocompletion

--- completions/vox.fish
+++ completions/vox.fish
@@ -1,6 +1,15 @@
 # Set up auto-completion for the vox command
 complete -c vox -f
-complete -c vox -n __fish_use_subcommand -a "new remove activate deactivate"
-complete -c vox -n "__fish_seen_subcommand_from remove" -a "(__vox_complete_envs)"
-complete -c vox -n "__fish_seen_subcommand_from activate" -a "(__vox_complete_envs)"
+
+# Main subcommands with descriptions
+complete -c vox -n __fish_use_subcommand -a "new" -d "Create a new virtual environment"
+complete -c vox -n __fish_use_subcommand -a "remove" -d "Remove an existing virtual environment"
+complete -c vox -n __fish_use_subcommand -a "activate" -d "Activate a virtual environment"
+complete -c vox -n __fish_use_subcommand -a "deactivate" -d "Deactivate the current virtual environment"
+
+# Environment name completions for remove and activate commands
+complete -c vox -n "__fish_seen_subcommand_from remove" -a "(__vox_complete_envs)" -d "Virtual environment"
+complete -c vox -n "__fish_seen_subcommand_from activate" -a "(__vox_complete_envs)" -d "Virtual environment"
+
+# New command should not complete files
 complete -c vox -n "__fish_seen_subcommand_from new" -f
